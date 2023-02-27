366125d402c36bad6f632c12e72706274a82d9fe
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-27 11:33:05 -0500

    feat: add initialize method for ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,17 @@
 class Ride
- 
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :total_revenue,
+              :rider_log
+
+  def initialize(details)
+    @name = details[:name]
+    @min_height = details[:min_height]
+    @admission_fee = details[:admission_fee]
+    @excitement = details[:excitement]
+    @total_revenue = 0
+    @rider_log = {}
+  end
 end
