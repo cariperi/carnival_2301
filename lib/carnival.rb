993d3d92ec6f4b476d93e55c47ae3b942d517ea4
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-27 13:32:35 -0500

    feat: add helper methods for get details method and build out get details

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -22,4 +22,48 @@ class Carnival
   def total_ride_revenue
     @rides.map{|ride| ride.total_revenue}.sum
   end
+
+  def get_details
+    details = {}
+    details[:visitor_count] = visitor_count
+    details[:revenue] = total_ride_revenue
+    details[:visitor_summary] = 0
+    details[:ride_summary] = 0
+    details
+  end
+
+  def visitor_count
+    @rides.map {|ride| ride.rider_log.keys}.uniq.count
+  end
+
+  def visitor_summary
+    summary = {}
+    visitors = @rides.map{|ride| ride.rider_log.keys}.flatten.uniq
+    visitors.each do |visitor|
+      summary[visitor] = [get_favorite_ride(visitor), total_spend(visitor)]
+    end
+    summary
+  end
+
+  def get_favorite_ride(visitor)
+    all_rides = @rides.map{|ride| ride if ride.rider_log.keys.include?(visitor)}
+    all_rides.delete(nil)
+    favorite_ride = all_rides[0]
+    all_rides.each do |ride|
+      if !ride.nil? && (ride.rider_log[visitor] > favorite_ride.rider_log[visitor])
+        favorite_ride = ride
+      end
+      favorite_ride
+    end
+    favorite_ride.name
+  end
+
+  def total_spend(visitor)
+    total_spend = 0
+    all_rides = @rides.map{|ride| ride if ride.rider_log.keys.include?(visitor)}
+    all_rides.each do |ride|
+      total_spend += (ride.rider_log[visitor] * ride.admission_fee) unless ride.nil?
+    end
+    total_spend
+  end
 end
